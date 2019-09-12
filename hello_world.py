5589eba1b09b1419795eba160be8a0e19c4968f1
Ubuntu <ubuntu@ip-172-31-16-225.ec2.internal>
2019-09-12 18:45:25 +0000

    commit message

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/user/bin/env python3
+
+# Created by: Jaeyoon
+# Created on: Sept 2019
+# This is the "Hello, World" program
+
+
+def main():
+    print("Hello, World!")
+    
+    
+if __name__ == "__main__":
+    main()
